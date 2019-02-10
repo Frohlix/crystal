5502d829bdb41df746b3b0a99f52f17aba5576e2
Felix Kehrer <felix.kehrer@gmail.com>
2019-02-11 00:27:30 +0100

    add _string macros to ECR module

---
+++ demonstration.cr
@@ -0,0 +1,14 @@
+require "ecr"
+
+class Greeter
+    def initialize(@name : String)
+    end
+
+    ECR.def_to_s_string "Hello <%= @name %>!"
+end
+
+puts Greeter.new("John").to_s_string
+
+othername = "Jack"
+
+puts ECR.render_string("Hello <%= othername %>!")
